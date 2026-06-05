b465542b5491ca68f41f1fc33df72a61ee16e66d
fabiohanada <fabiohanada23@gmail.com>
2026-06-05 09:17:38 -0300

    Atualiza funcao para calculos dos valores

--- myapp/models.py
+++ myapp/models.py
@@ -71,9 +71,21 @@ class MontaPote(models.Model):
     embalagem = models.ForeignKey(Embalagem, related_name='embalagem', on_delete=models.CASCADE)
     coberturas = models.ManyToManyField(Cobertura)
     quantidade = models.PositiveIntegerField(null=True)
+
+    def preco_total(self):
+        preco_embalagem = self.embalagem.preco if self.embalagem else 0
+        preco_coberturas = sum(cobertura.preco for cobertura in self.coberturas.all())
+        preco_sabores = 0
+        for selsabor in self.pote.all():
+            preco_sabor = selsabor.sabor.tipo.preco
+            quantidade_bolas = selsabor.quantidade_bolas
+            preco_sabores += preco_sabor * quantidade_bolas
+        total_pote = preco_embalagem + preco_coberturas + preco_sabores
+        total = total_pote * self.quantidade 
+        return total   
     
     def __str__(self):          
-        return f"ID: {self.id} / POTE: {self.embalagem.tipo} / Qtd: {self.quantidade}"
+        return f"ID: {self.id} / POTE: {self.embalagem.tipo} / Qtd: {self.quantidade} / {self.preco_total()}"
 
     class Meta:
         verbose_name = 'B - MontaPote'
@@ -103,10 +115,19 @@ class SacolaItens(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     
     def preco_formatado(self):
-        return f'R$ {self.preco:.2f}'   
+        return f'R$ {self.preco:.2f}' 
+
+    def preco_total(self):
+        # Calcule a soma dos preços de todos os potes na sacola (Itens)
+        sacola_total = 0
+        for pote in self.potes.all():
+            sacola_total += pote.preco_total()
+        self.preco = sacola_total
+        self.save()
+        return sacola_total  
     
     def __str__(self):
-        return f"CARINHO {self.id}"
+        return f"CARINHO {self.id} / {self.preco_total()}"
     
     class Meta:
         verbose_name = 'C - SacolaItens'
